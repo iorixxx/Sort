742929a188815750f716982c5df98cd9418e736a
Ahmet Arslan <iorixxx@yahoo.com>
2020-10-19 15:20:07 +0300

    mavenization is now completed

---
+++ src/main/java/edu/eskisehir/Sort.java
@@ -0,0 +1,36 @@
+package edu.eskisehir;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import java.nio.charset.StandardCharsets;
+
+// suppose a Java application called Sort sorts lines in a file.
+public class Sort {
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            System.out.println("please supply an argument: file name");
+            return;
+        }
+
+        Path path = Paths.get(args[0]);
+
+        if (!Files.exists(path)) {
+            System.out.println("no such file : " + path.toString());
+            return;
+        }
+
+        List<String> lines = Files.readAllLines(path, StandardCharsets.US_ASCII);
+
+        Collections.sort(lines);
+
+        for (String s : lines)
+            System.out.println(s);
+        
+    }
+}
